365e227bc5fc8175b5997383a90d2a44a0f91def
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-04-03 19:29:20 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
   def change
-    add_column :students, :grade,
+    add_column :students, :grade, :integer
     add_column :birthdate
     
   end
